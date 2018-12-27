1714a68107fefff787fca3c0771b4b1d5fbd358c
JunnosukeNagano <nagano@clubnets.jp>
2018-12-27 11:42:13 +0900

    add testCode

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,6 +5,9 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+use Illuminate\Support\Facades\Redis;
+
 class ExampleTest extends TestCase
 {
     /**
@@ -17,5 +20,33 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    
+    public function testCreateHDDUser()
+    {
+        $time_start = microtime(true);
+        for($i=0; $i<10000; $i++){
+            $user = new User();
+            $user->email = 'naganohdd_'.$i.'@clubnets.jp';
+            $user->name = 'テストHDDユーザー'.$i;
+            $user->password = '$2y$10$xR6MeDVlxJxFtNFeYVb/p.KHNQaTf5Rkn6eJBX2ltCKOLVnDv0w8G';
+            $user->save();
+        }
+        $time = microtime(true) - $time_start;
+        echo "{$time} 秒";
+    }
+    
+    public function testCreateRedisUser()
+    {
+        $time_start = microtime(true);
+        for($i=0; $i<10000; $i++){
+            $user = new User();
+            $user->email = 'naganoredis_'.$i.'@clubnets.jp';
+            $user->name = 'テストRedisユーザー'.$i;
+            $user->password = '$2y$10$xR6MeDVlxJxFtNFeYVb/p.KHNQaTf5Rkn6eJBX2ltCKOLVnDv0w8G';
+            Redis::command('SET', ['user-'.$i, json_encode($user, JSON_UNESCAPED_UNICODE)]);
+        }
+        $time = microtime(true) - $time_start;
+        echo "{$time} 秒";
     }
 }
